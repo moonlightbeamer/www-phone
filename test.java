a21302f44ce6b5b5225999d0ca6886ab4d86ddbd
Lin Zhu <moonlightbeamer@ymail.com>
2023-07-13 18:31:57 -0600

    update

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class test {
+    public static void main(String[] args) {
+        Scanner hp = new Scanner(System.in);
+        System.out.println("Input an integer:\n");
+        int val = hp.nextInt();
+        System.out.println(val);
+        hp.close();
+    }
+}
